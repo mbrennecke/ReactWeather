1184f38b7e8cf02cfbf35a90b87ab822701a4020
Matthew Brennecke <matthew.brennecke@roundtower.com>
2016-06-06 13:37:00 -0400

    Add foundation and jQuery

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -13,8 +28,8 @@ module.exports = {
 	  About: 'app/components/About.jsx',
 	  Examples: 'app/components/Examples.jsx',
 	  WeatherForm: 'app/components/WeatherForm.jsx',
-	  WeatherMessage: 'app/components/WeatherMessage.jsx',	
-	openWeatherMap: 'app/api/openWeatherMap.jsx'
+	  WeatherMessage: 'app/components/WeatherMessage.jsx',
+	   openWeatherMap: 'app/api/openWeatherMap.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
