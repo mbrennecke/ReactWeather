61c7526fb344ad126d7b1490ce8087f43c59f462
Matthew Brennecke <matthew.brennecke@roundtower.com>
2016-06-06 14:31:35 -0400

    Style About page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,16 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to about page!</p>
+			<h1 className="text-center">About</h1>
+			<p>This app is an example of a React built web app, styled with Foundation, that displays weather for a requested city.</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react/">Link to React</a>
+				</li>
+				<li>
+					<a href="http://foundation.zurb.com/">Link to Foundation</a>
+				</li>
+			</ul>
 		</div>
 		)
 };
